ddea00d751c5daf78b1dd793b4332b7e9131fe0e
Your Name <mallapukishor143@gmail.com>
2025-08-17 16:50:51 +0530

    todo app

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,10 @@ from todo.models import Task
 def home(request):
     #return HttpResponse('<h1>Homepage</h1>')
     tasks= Task.objects.filter(is_completed=False).order_by('updated_at')
+    completed_tasks=Task.objects.filter(is_completed=True)
+    print(completed_tasks)
     context={
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request,'home.html',context)
